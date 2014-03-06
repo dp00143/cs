cfc9fd97f294355d3be81de089f06727692993dd
daphizzle <daniel.puschmann@gmx.de>
2014-03-06 21:33:30 +0000

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,16 @@
+__author__ = 'Daniel'
+
+import trafficDataImport
+import clusterTrafficData
+import threading
+
+ch1 = "traffic3"
+t1 = threading.Thread(target=trafficDataImport.importAllData, args=(ch1,))
+t2 = threading.Thread(target=clusterTrafficData.clusterData, args=(ch1, 1))
+
+
+print "starting t1"
+t2.start()
+print "starting t2"
+t1.start()
+print "both threads started"
