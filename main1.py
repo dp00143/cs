e8623a2f2e614d9545c147ba132b7b45f00a45d4
daphizzle <daniel.puschmann@gmx.de>
2014-03-25 17:41:23 +0000

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,16 +1,15 @@
 __author__ = 'Daniel'
 
-import trafficDataImport
 import clusterTrafficData
 import threading
 
 ch1 = "traffic2"
-t1 = threading.Thread(target=trafficDataImport.importAllData, args=(ch1,))
+# t1 = threading.Thread(target=trafficDataImport.importAllData, args=())
 t2 = threading.Thread(target=clusterTrafficData.clusterData, args=(ch1, 3))
 
 
-print "starting t1"
+print "starting cluster for 2"
 t2.start()
-print "starting t2"
-t1.start()
-print "both threads started"
+# print "starting t2"
+# t1.start()
+# print "both threads started"
